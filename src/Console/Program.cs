b4e7358e9db3c3dfeb9072665c5de8d6bb127499
ASwitchCase <47430750+ASwitchCase@users.noreply.github.com>
2023-11-07 12:35:41 -0500

    feat: add jw library startup code (#8)

--- src/Console/Program.cs
+++ src/Console/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+ProcessStartInfo startInfo = new ProcessStartInfo
+{
+    FileName = "explorer.exe",
+    Arguments = $"shell:AppsFolder\\WatchtowerBibleandTractSo.45909CDBADF3C_5rz59y55nfz3e!App",
+};
+
+Process.Start(startInfo);
+
+
